0fd566429a49a8f5948d9ebf6a84de626222bd6f
zhouyouenjoy <zhouyoubnu@gmail.com>
2025-09-30 22:42:06 +0800

    71

--- event/urls.py
+++ event/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.event_list, name='event_list'),
     path('<int:pk>/', views.event_detail, name='event_detail'),
     path('create/', views.create_event, name='create_event'),
+    path('edit/<int:event_id>/', views.create_event, name='edit_event'),
     path('register/<int:session_id>/', views.register_session, name='register_session'),
     path('my-events/', views.my_events, name='my_events'),
     path('registrations/<int:event_id>/', views.event_registrations, name='event_registrations'),  # 添加查看报名名单的URL
--- event/views.py
+++ event/views.py
@@ -99,15 +99,22 @@ def model_album(request, model_id):
 
 
 @login_required
-def create_event(request):
-    """创建摄影活动"""
+def create_event(request, event_id=None):
+    """创建或编辑摄影活动"""
+    # 检查是否是编辑模式
+    is_edit = event_id is not None
+    if is_edit:
+        event = get_object_or_404(Event, pk=event_id, created_by=request.user)
+    else:
+        event = None
+    
     if request.method == 'POST':
-        form = EventForm(request.POST, request.FILES)
+        form = EventForm(request.POST, request.FILES, instance=event)
         if form.is_valid():
             event = form.save(commit=False)
             event.created_by = request.user
-            # 默认需要审核
-            event.approved = False
+            # 默认设置为已审核通过状态
+            event.approved = True
             
             # 处理地理位置信息
             if 'location_poi' in request.POST:
@@ -126,86 +133,150 @@ def create_event(request):
             # 保存活动对象
             event.save()
             
-            # 处理模特信息
-            model_count = 0
-            for key in request.POST.keys():
-                if key.startswith('model_name_'):
-                    model_count += 1
-                    model_name = request.POST.get(f'model_name_{model_count}')
-                    model_fee = request.POST.get(f'model_fee_{model_count}')
-                    model_vip_fee = request.POST.get(f'model_vip_fee_{model_count}')
+            # 如果是编辑模式，处理模特信息的更新
+            if is_edit:
+                # 处理现有模特的更新
+                for model in event.models.all():
+                    # 检查是否有删除图片的标记
+                    if request.POST.get(f'delete_model_images_{model.id}'):
+                        model.model_images = None
+                    if request.POST.get(f'delete_outfit_images_{model.id}'):
+                        model.outfit_images = None
+                    if request.POST.get(f'delete_scene_images_{model.id}'):
+                        model.scene_images = None
                     
-                    if model_name and model_fee:
-                        # 创建模特
-                        event_model = EventModel(
-                            event=event,
-                            name=model_name,
-                            fee=model_fee
-                        )
-                        
-                        # 设置VIP价格（如果提供）
-                        if model_vip_fee:
-                            event_model.vip_fee = model_vip_fee
-                        
-                        # 处理模特用户ID
-                        model_user_id = request.POST.get(f'model_user_{model_count}')
+                    # 处理新上传的图片
+                    model_images = request.FILES.getlist(f'model_images_{model.id}')
+                    if model_images:
+                        model.model_images = model_images[0]
+                    
+                    outfit_images = request.FILES.getlist(f'outfit_images_{model.id}')
+                    if outfit_images:
+                        model.outfit_images = outfit_images[0]
+                    
+                    scene_images = request.FILES.getlist(f'scene_images_{model.id}')
+                    if scene_images:
+                        model.scene_images = scene_images[0]
+                    
+                    # 更新其他字段
+                    model_name_key = f'model_name_{model.id}'
+                    model_fee_key = f'model_fee_{model.id}'
+                    model_vip_fee_key = f'model_vip_fee_{model.id}'
+                    model_photographer_count_key = f'model_photographer_count_{model.id}'
+                    model_user_key = f'model_user_{model.id}'
+                    
+                    if model_name_key in request.POST:
+                        model.name = request.POST[model_name_key]
+                    
+                    if model_fee_key in request.POST:
+                        model.fee = request.POST[model_fee_key]
+                    
+                    if model_vip_fee_key in request.POST:
+                        vip_fee = request.POST[model_vip_fee_key]
+                        model.vip_fee = vip_fee if vip_fee else None
+                    
+                    if model_user_key in request.POST:
+                        model_user_id = request.POST[model_user_key]
                         if model_user_id:
                             try:
-                                event_model.model_user = User.objects.get(id=model_user_id)
+                                model.model_user = User.objects.get(id=model_user_id)
                             except (User.DoesNotExist, ValueError):
-                                event_model.model_user = None
+                                model.model_user = None
                         else:
-                            event_model.model_user = None
-                        
-                        event_model.save()
-                        
-                        # 处理模特照片上传
-                        model_images = request.FILES.getlist(f'model_images_{model_count}')
-                        if model_images:
-                            # 保存第一张图片到model_images字段
-                            event_model.model_images = model_images[0]
-                        
-                        # 处理模特服装图片上传
-                        outfit_images = request.FILES.getlist(f'outfit_images_{model_count}')
-                        if outfit_images:
-                            # 保存第一张图片到outfit_images字段
-                            event_model.outfit_images = outfit_images[0]
-                        
-                        # 处理拍摄场景图片上传
-                        scene_images = request.FILES.getlist(f'scene_images_{model_count}')
-                        if scene_images:
-                            # 保存第一张图片到scene_images字段
-                            event_model.scene_images = scene_images[0]     
-                        event_model.save()
-                        
-                        # 处理场次信息
-                        session_count = 0
-                        # 获取该模特的photographer_count
-                        model_photographer_count = request.POST.get(f'model_photographer_count_{model_count}', 1)
-                        
-                        for session_key in request.POST.keys():
-                            if session_key.startswith(f'start_time_{model_count}_'):
-                                session_count += 1
-                                start_time = request.POST.get(f'start_time_{model_count}_{session_count}')
-                                end_time = request.POST.get(f'end_time_{model_count}_{session_count}')
-                                
-                                if start_time and end_time:
-                                    EventSession.objects.create(
-                                        model=event_model,
-                                        title=f'场次 {session_count}',
-                                        start_time=start_time,
-                                        end_time=end_time,
-                                        photographer_count=model_photographer_count
-                                    )
+                            model.model_user = None
+                    
+                    model.save()
             
-            messages.success(request, '活动创建成功，等待管理员审核！')
+            # 如果是创建模式，处理新模特的创建
+            if not is_edit:
+                # 处理模特信息
+                model_count = 0
+                for key in request.POST.keys():
+                    if key.startswith('model_name_'):
+                        model_count += 1
+                        model_name = request.POST.get(f'model_name_{model_count}')
+                        model_fee = request.POST.get(f'model_fee_{model_count}')
+                        model_vip_fee = request.POST.get(f'model_vip_fee_{model_count}')
+                        
+                        if model_name and model_fee:
+                            # 创建模特
+                            event_model = EventModel(
+                                event=event,
+                                name=model_name,
+                                fee=model_fee
+                            )
+                            
+                            # 设置VIP价格（如果提供）
+                            if model_vip_fee:
+                                event_model.vip_fee = model_vip_fee
+                            
+                            # 处理模特用户ID
+                            model_user_id = request.POST.get(f'model_user_{model_count}')
+                            if model_user_id:
+                                try:
+                                    event_model.model_user = User.objects.get(id=model_user_id)
+                                except (User.DoesNotExist, ValueError):
+                                    event_model.model_user = None
+                            else:
+                                event_model.model_user = None
+                            
+                            event_model.save()
+                            
+                            # 处理模特照片上传
+                            model_images = request.FILES.getlist(f'model_images_{model_count}')
+                            if model_images:
+                                # 保存第一张图片到model_images字段
+                                event_model.model_images = model_images[0]
+                            
+                            # 处理模特服装图片上传
+                            outfit_images = request.FILES.getlist(f'outfit_images_{model_count}')
+                            if outfit_images:
+                                # 保存第一张图片到outfit_images字段
+                                event_model.outfit_images = outfit_images[0]
+                            
+                            # 处理拍摄场景图片上传
+                            scene_images = request.FILES.getlist(f'scene_images_{model_count}')
+                            if scene_images:
+                                # 保存第一张图片到scene_images字段
+                                event_model.scene_images = scene_images[0]     
+                            event_model.save()
+                            
+                            # 处理场次信息
+                            session_count = 0
+                            # 获取该模特的photographer_count
+                            model_photographer_count = request.POST.get(f'model_photographer_count_{model_count}', 1)
+                            
+                            for session_key in request.POST.keys():
+                                if session_key.startswith(f'start_time_{model_count}_'):
+                                    session_count += 1
+                                    start_time = request.POST.get(f'start_time_{model_count}_{session_count}')
+                                    end_time = request.POST.get(f'end_time_{model_count}_{session_count}')
+                                    
+                                    if start_time and end_time:
+                                        EventSession.objects.create(
+                                            model=event_model,
+                                            title=f'场次 {session_count}',
+                                            start_time=start_time,
+                                            end_time=end_time,
+                                            photographer_count=model_photographer_count
+                                        )
+            
+            messages.success(request, '活动创建成功！' if not is_edit else '活动更新成功！')
             return redirect('event:event_list')
     else:
-        form = EventForm()
+        form = EventForm(instance=event)
+    
+    # 获取现有模特信息（用于编辑模式）
+    existing_models = []
+    if is_edit and event:
+        existing_models = EventModel.objects.filter(event=event).prefetch_related('sessions')
     
     return render(request, 'event/create_event.html', {
         'form': form,
-        'ak': '46xD48lIm4oyiWq1RaKyxhr2ZhkZiCWg'
+        'ak': '46xD48lIm4oyiWq1RaKyxhr2ZhkZiCWg',
+        'is_edit': is_edit,
+        'event': event,
+        'existing_models': existing_models
     })
 
 
